201e795848a81ae03cb82bc1145a1dc3babcad1b
Julian-Carlosama <cromocelcarlosama@gmail.com>
2022-04-21 19:22:15 -0500

    New exercises for interview in Python

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+from itertools import dropwhile
+
+
+def canUnlockAll(boxes):
+    """Method that determines if all the boxes can be opened."""
+
+    keys = {0}
+    boxesRange = range(len(boxes))
+
+    while True:
+        not_found = set()
+
+        for i in boxesRange:
+            if i in keys:
+                for key in dropwhile(lambda k: k in keys, boxes[i]):
+                    keys.add(key)
+            else:
+                not_found.add(i)
+
+        if not not_found:
+            return True
+
+        if boxesRange == not_found:
+            return False
+
+        boxesRange = not_found
