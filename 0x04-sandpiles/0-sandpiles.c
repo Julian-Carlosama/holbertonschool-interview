dd0185769c5a526106683a16fac94c37c729e07a
Julian-Carlosama <cromocelcarlosama@gmail.com>
2022-05-19 23:39:08 -0500

    Fix Betty v3

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -102,7 +102,7 @@ void toppling(int grid[3][3])
  */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-        grid_sum(grid1, grid2);
+	grid_sum(grid1, grid2);
 	while (!is_stable(grid1))
 	{
 		printf("=\n");
