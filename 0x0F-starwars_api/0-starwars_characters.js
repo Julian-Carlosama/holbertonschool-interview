ced6f694652b2d4bfeddaddab7f844b30b68f6b9
Julian-Carlosama <cromocelcarlosama@gmail.com>
2022-08-11 17:37:53 -0500

    Update task v1

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,2 +1,14 @@
 #!/usr/bin/node
 
+const req = require('request');
+const urlFilm = 'https://swapi-api.hbtn.io/api/';
+
+async function getCharactersSWm () {
+  const response = await FunctionNme(url);
+  for (let i = 0; i < response.characters; i++) {
+    const getCh = await getRequest(response.characters[i]);
+    console.log(getCh.name);
+  }
+}
+
+getCharactersSWm();
