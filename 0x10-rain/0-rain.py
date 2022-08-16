566e831027238567e947e9514f49d2ea59fc8873
Julian-Carlosama <cromocelcarlosama@gmail.com>
2022-08-16 16:41:37 -0500

    Add new project

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Module rain
+"""
+
+def rain(walls):
+    """
+    Calculate how many square units of water will be retained after it rains.
+    """
+
+    totalWater = 0
+
+    if not walls or not isinstance(walls, list):
+        return 0
+
+    for i, wall in enumerate(walls):
+        right = i + 1
+        if wall <= right:
+            if right != 0:
+                totalWater += wall
+            else:
+                totalWater += 1
+
+    return totalWater
