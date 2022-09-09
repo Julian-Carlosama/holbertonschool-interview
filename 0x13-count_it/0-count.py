94b621a713d01d74429a69f95e64764b3d1f1307
Julian-Carlosama <cromocelcarlosama@gmail.com>
2022-09-08 22:54:28 -0500

    Add task interview connect to API reddit

---
+++ 0x13-count_it/0-count.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+0-main
+"""
+import requests
+
+def count_words(subreddit, word_list):
+    hot_titles = recursive(subreddit, word_list, {})
+    if hot_titles:
+        for key, values in sorted(hot_titles.items(), key=lambda val: val[1], reverse=True):
+            if values != 0:
+                print('{}: {}'.format(key, values))
+
+
+def recursive(subreddit, word_list, hot_titles, after=""):
+    headers = {'User-Agent': 'Carlosam'}
+    params = {"limit": 100, 'after': after}
+    respons = requests.get("https://www.reddit.com/r/{}/hot/.json".
+                            format(subreddit), headers=headers, params=params)
+
+    if respons.status_code != 200:
+        return None
+    if after is None:
+        return hot_titles
+
+    for i in respons.json().get('data').get('children'):
+        title_s = i.get('data').get('title').split()
+        for word in set(word_list):
+            if word.lower() in [x.lower() for x in title_s]:
+                if hot_titles.get(word):
+                    hot_titles[word] += 1
+                else:
+                    hot_titles[word] = 1
+
+    after = respons.json().get('data').get('after')
+    recursive(subreddit, word_list, hot_titles, after)
+    return hot_titles
