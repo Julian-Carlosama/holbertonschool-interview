3f2c4daf4f0be4953b880bbc436031eebec44f0f
JulianCarlosama <cromocelcarlosama@gmail.com>
2022-09-29 12:39:16 -0500

    Update task

--- 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 """
-Test 0x16 - Rotate 2D Matrix
+0x16 - function Rotate 2D Matrix 
 """
-def rotate_2d_matrix(matrix):
 
+
+def rotate_2d_matrix(matrix):
+    matrixCopy = matrix.copy()
+    lenM = len(matrix)
+
+    for i in range(lenM):
+        matrix[i] = [row[i] for row in matrixCopy]
