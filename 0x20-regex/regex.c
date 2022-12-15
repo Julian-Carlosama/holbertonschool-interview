9e0307459a11b9dbaf65496efd583d7bccb8b21e
Julian-Carlosama <cromocelcarlosama@gmail.com>
2022-12-15 15:19:28 -0500

    Betty style Fixed

--- 0x20-regex/regex.c
+++ 0x20-regex/regex.c
@@ -11,15 +11,15 @@
 
 int regex_match(char const *str, char const *pattern)
 {
-        int ptr = 0;
+	int ptr = 0;
 
-        if (*str == '\0' && *pattern == '\0')
-                return 1;
-        
-        if ((*str == *pattern || *pattern == '.') && *(pattern + 1) != '*')
-                return (regex_match(str + 1, pattern + 1));
-        
-        if (*(pattern + 1) == '*')
+	if (*str == '\0' && *pattern == '\0')
+		return (1);
+
+	if ((*str == *pattern || *pattern == '.') && *(pattern + 1) != '*')
+		return (regex_match(str + 1, pattern + 1));
+
+	if (*(pattern + 1) == '*')
 	{
 		if (*str != '\0' && (*str == *pattern || *pattern == '.'))
 			ptr = regex_match(str + 1, pattern);
