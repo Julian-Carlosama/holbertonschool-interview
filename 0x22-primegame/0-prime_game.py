98ac91b00fe8fb3bf6339fd1b6d7351ee8377a56
Julian-Carlosama <cromocelcarlosama@gmail.com>
2023-01-05 20:54:09 -0500

    Update pycodestyle

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -11,17 +11,17 @@ def isWinner(x, nums):
         numbers = set(i for i in range(1, nums[round] + 1))
         """Mark all multiples of primes as non-prime"""
         i = 2
-        
+
         while i <= nums[round]:
             """Remove all multiples of i from the set"""
             numbers.difference_update(set(range(i, nums[round] + 1, i)))
 
             """Find the next prime number"""
             i = min(numbers, default=nums[round] + 1)
-        
+
         """Count the number of prime numbers"""
         prime_count = len(numbers)
-        
+
         """Determine the winner"""
         if prime_count % 2 == 0:
             winner = "Ben"
